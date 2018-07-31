a8436565fbbd40d22fa120120936f44970a510dd
Ivan Abramenko <ivan.abramenko@technocrat.com.au>
2018-07-31 13:10:54 +0600

    Add page programmatically

---
+++ modules/custom/drupalbook/src/Controller/FirstPageController.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drupalbook\Controller\FirstPageController.
+ */
+
+namespace Drupal\drupalbook\Controller;
+
+/**
+ * Provides route responses for the DrupalBook module.
+ */
+class FirstPageController {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function content() {
+    $element = array(
+      '#markup' => 'Hello World!',
+    );
+    return $element;
+  }
+
+}

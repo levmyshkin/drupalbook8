05d13dae6169e25b4d543b47c63f164d685483e4
Ivan Abramenko <ivan@829llc.com>
2021-11-06 14:05:15 +0300

    9.9.2. Config Split, Config Ignore. Creating various configs for different instances.

---
+++ settings.local.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['config_split.config_split.local']['status'] = TRUE;
+
+// Dev.
+// $config['config_split.config_split.dev']['status'] = TRUE;
+
+// Live.
+// $config['config_split.config_split.live']['status'] = TRUE;
